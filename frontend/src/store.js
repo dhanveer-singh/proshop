8485e8f5c9d3c9c1ac83eb8821d233292db73016
dhanveer-singh <singhdhanveer007@gmail.com>
2023-01-14 21:36:06 +0530

    Order Pay Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers.js';
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers.js';
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers.js';
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers.js';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -15,7 +15,8 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
-    orderDetails: orderDetailsReducer
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer
 
 });
 
