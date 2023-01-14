8efd7bb2677637b2c6c07a3e29603490af33f31c
dhanveer-singh <singhdhanveer007@gmail.com>
2023-01-14 17:00:54 +0530

    Order Details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducer, productDetailsReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers.js';
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducers.js';
-import { orderCreateReducer } from './reducers/orderReducers.js';
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers.js';
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -15,6 +15,7 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
     orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer
 
 });
 
